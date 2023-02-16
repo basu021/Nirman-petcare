1532f7a1cf6c86f130c9725a9bb810b093f0ab2e
ashu12355 <sharmaashutosh12355@gmail.com>
2023-02-16 23:12:48 +0530

    header done

--- view/header/TopHeader.php
+++ view/header/TopHeader.php
@@ -6,23 +6,15 @@
                 <ul class="header-top-info-list">
                     <li>
                         <span class="icon"><i class="icofont-ui-call"></i></span>
-                        <a href="tel://5123602763" class="text">(512) 360-2763</a>
+                        <a href="tel://+917798789898" class="text">+917798789898</a>
                     </li>
                     <li>
                         <span class="icon"><i class="icofont-envelope-open"></i></span>
-                        <a href="mailto://support@gamil.com" class="text">support@gamil.com</a>
+                        <a href="mailto://support@gamil.com" class="text">contact@petcare.com</a>
                     </li>
                     <li>
                         <span class="icon"><i class="icofont-google-map"></i></span>
-                        <span class="text">Smithville, Texas(TX), 78957</span>
-                    </li>
-                </ul>
-            </div>
-            <div class="col-auto">
-                <ul class="header-top-info-list">
-                    <li>
-                        <span class="icon"><i class="icofont-clock-time"></i></span>
-                        <span class="text">Sun to Thu - 08:00 am to 09:00 pm</span>
+                        <span class="text">Technology Corridor, Infocity, Bhubaneswar, Odisha</span>
                     </li>
                 </ul>
             </div>
