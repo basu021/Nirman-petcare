d7509d250d26faf7b217cd3359e2fd7e7bc9a414
ashu12355 <sharmaashutosh12355@gmail.com>
2023-02-17 01:36:53 +0530

    ..

---
+++ view/adoption/adoption.php
@@ -0,0 +1,64 @@
+<?php include "../header/Header.php"; ?>
+<!--== Start: Wrapper ==-->
+<div class="wrapper">
+    <?php include "../header/TopHeader.php"; ?>
+    <?php include "../header/HeaderMenus.php"; ?>
+
+
+
+
+
+   <!--== Start: About Section Wrapper ==-->
+   <div class="about-section section section-margin pt-0 pb-0 pt-xl-5 pb-xl-5">
+            <div class="container">
+                <div class="row mt-n1 pt-0 pt-lg-5 pt-xl-10 pb-6 pb-md-10 pb-lg-5 pb-xl-10 mb-5 mb-lg-0">
+                    <div class="col-lg-5">
+                        <!--== Section Title Start ==-->
+                        <div class="section-title-2 mb-0">
+                            <h2 class="title">Save animals, Save future.</h2>
+                            <p>It is a long established fact that reader will be distracted by the readable content of a page when looking at it layout using normal distribution opposed.</p>
+                            <p>It is a long established fact that reader will be distracted by the readable content of a page when looking at it layout using normal distribution opposed.</p>
+                        </div>
+                        <!--== Section Title End ==-->
+                    </div>
+                </div>
+            </div>
+            <div class="section-bg-img section-bg-img-style1" data-bg-img="../assets/images/photos/about1.jpg"></div>
+            <div class="section-pattern-img section-pattern-img-style2 pattern-img-move">
+                <img src="../assets/images/shape/2.png" width="148" height="190" alt="Image">
+            </div>
+        </div>
+        <!--== End: About Section Wrapper ==-->
+    <!--== Start: Event Section Wrapper ==-->
+    <div class="event-section section section-padding">
+        <div class="container">
+
+
+            <div class="row mb-n6">
+                <!--== Start: Event Title ==-->
+                <div class="col-12 mb-6">
+                    <div class="event-item  ">
+                        <a href="event-details.html" class="image">
+                            <img src="../assets/images/event/event-1.jpg" width="350" height="315"
+                                alt="Fund raising for save forest.">
+                        </a>
+                        <div class="content">
+                            <div class="details">
+                                <span class="location"><span>Venue:</span> Smithville, Texas(TX), 78957</span>
+                                <h4 class="title"><a href="event-details.html">Fund raising for save forest.</a></h4>
+                                <p>That neces ecommerce platform optime your store ecommerce platform</p>
+                                <span class="date"><span>Date:</span> 30 February, 2021</span>
+                            </div>
+                            <div class="button">
+                                <a class="btn btn-primary btn-icon-right btn-lg" href="event-details.html"><span>Join
+                                        Now</span> <i class="icofont-double-right icon"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--== End: Event Title ==-->
+            </div>
+        </div>
+    </div>
+    <!--== End: Event Section Wrapper ==-->
+    <?php include "../footer/Footer.php"; ?>
