f339b95ad9956780a9c4a828af88a96fa82e86d9
santos021 <santosbiswal543@gmail.com>
2023-02-17 04:05:19 +0530

    servies updates

--- view/services/index.php
+++ view/services/index.php
@@ -14,12 +14,12 @@
             <div class="container">
                 <div class="row justify-content-between align-items-center">
                     <div class="col-sm-auto text-center text-sm-start">
-                        <h1 class="page-header-title">About Us</h1>
+                        <h1 class="page-header-title">Services</h1>
                     </div>
                     <div class="col-sm-auto">
                         <ol class="breadcrumb mb-0 justify-content-center mt-3 mt-sm-0">
                             <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-                            <li class="breadcrumb-item active" aria-current="page">About Us</li>
+                            <li class="breadcrumb-item active" aria-current="page">Services</li>
                         </ol>
                     </div>
                 </div>
@@ -28,8 +28,8 @@
             <!--== End: Page Header Area Wrapper ==-->
 
 
-
-
+<br><br>
+<h1 style="text-align:center">Doctor Consultant</h1>
 <div class="event-section section section-padding">
             <div class="container">
                
@@ -37,15 +37,34 @@
                     <!--== Start: Event Title ==-->
                     <div class="col-12 mb-6">
                         <div class="event-item  ">
-                            <a href="event-details.html" class="image">
+                            <a href="#" class="image">
                                 <img src="../assets/images/event/event-1.jpg" width="350" height="315" alt="Fund raising for save forest.">
                             </a>
                             <div class="content">
                                 <div class="details">
-                                    <span class="location"><span>Addres:</span> Chandrasekharpur, Bhubanwswar</span>
-                                    <h4 class="title"><a href="event-details.html">Name: Dr. Amit Kumar (M.B.B.S.)</a></h4>
+                                    <span class="location"><span>Addres:</span> Chandrasekharpur, Bhubaneswar</span>
+                                    <h4 class="title"><a href="#">Name: Dr. Amit Kumar (BVSc)</a></h4>
                                     <p>Dr. Amit Kumar is a veterinary doctor having 15 years of experience in cousulting.</p>
-                                    <span class="date"><
+                                    <span class="date">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!--== End: Event Title ==-->
+
+                     <!--== Start: Event Title ==-->
+                     <div class="col-12 mb-6">
+                        <div class="event-item  ">
+                            <a href="#" class="image">
+                                <img src="../assets/images/event/event-1.jpg" width="350" height="315" alt="Fund raising for save forest.">
+                            </a>
+                            <div class="content">
+                                <div class="details">
+                                    <span class="location"><span>Addres:</span> Chandrasekharpur, Bhubaneswar</span>
+                                    <h4 class="title"><a href="#">Name: Dr. Santos Biswal (Veterinary surgery and radiology
+)</a></h4>
+                                    <p>Dr. Santos Biswal is a veterinary doctor having 15 years of experience in cousulting.</p>
+                                    <span class="date">
                                 </div>
                             </div>
                         </div>
