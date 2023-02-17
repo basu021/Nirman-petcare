5842c48e4d9c26928525204264e343a49adbdc86
santos021 <santosbiswal543@gmail.com>
2023-02-17 05:33:03 +0530

    ........

--- view/home/index.php
+++ view/home/index.php
@@ -200,11 +200,8 @@ and pet owners to connect, and share information, and resources.</p>
             </div>
             <div class="section-bg-img section-bg-img-style2" data-bg-img="../assets/images/photos/bg1.jpg"></div>
         </div>
-<<<<<<< HEAD
     </div>
  </div>
-
-=======
         
         <!--== Start: Testimonial Section Wrapper ==-->
         <div class="testimonial-section section section-padding-t bg-light">
@@ -258,77 +255,12 @@ and pet owners to connect, and share information, and resources.</p>
                                         </div>
                                     </div>
                                 </div>
-                                <!--== End: Testimonial Title ==-->
-
-                                
-
-        <!--== Start: News Post Section Wrapper ==-->
-        <!-- <div class="blog-post-section section section-padding">
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-6 text-center text-sm-start"> -->
-                        <!--== Start: Section Title ==-->
-                        <!-- <div class="section-title mt-n1">
-                            <h2 class="sub-title">BLOG POST</h2>
-                            <h2 class="title mb-n3">Latest News</h2>
-                        </div> -->
-                        <!--== End: Section Title ==-->
-                    <!-- </div>
-                    <div class="col-sm-6 mb-10 mb-sm-0 mt-0 mt-sm-2 mt-xl-4 text-center text-sm-end">
-                        <a class="btn-link" href="blog.html">Other’s Blog Post <i class="icofont-double-right icon"></i></a>
-                    </div>
-                </div>
-
-                <div class="row row-gutter-60 mb-n6"> -->
-                    <!--== Start: News Post Item ==-->
-                    <!-- <div class="col-md-6 mb-6">
-                        <div class="post-item">
-                            <a href="blog-details.html" class="image">
-                                <img src="../assets/images/blog/image-1.jpg" width="475" height="475" alt="News Post Image">
-                            </a>
-                            <div class="content">
-                                <ul class="post-meta">
-                                    <li class="post-date"><span>Date:</span> 30 February, 2021</li>
-                                    <li class="post-info">
-                                        <ul class="d-flex">
-                                            <li class="post-comment"><span>Comment:</span> 8,962</li>
-                                            <li class="post-like ms-5"><span>Like:</span> 78K</li>
-                                        </ul>
-                                    </li>
-                                </ul>
-                                <h4 class="title"><a href="blog-details.html">Best 58 charity article of 2021</a></h4>
-                                <p>Lorem Ipsum is simply dummy textes of them printing and typesetting industry standard</p>
-                            </div>
+                            </div>              
                         </div>
-                    </div> -->
-                    <!--== End: News Post Item ==-->
-
-                    <!--== Start: News Post Item ==-->
-                    <!-- <div class="col-md-6 mb-6">
-                        <div class="post-item">
-                            <a href="blog-details.html" class="image">
-                                <img src="../assets/images/blog/image-2.jpg" width="475" height="475" alt="News Post Image">
-                            </a>
-                            <div class="content">
-                                <ul class="post-meta">
-                                    <li class="post-date"><span>Date:</span> 30 February, 2021</li>
-                                    <li class="post-info">
-                                        <ul class="d-flex">
-                                            <li class="post-comment"><span>Comment:</span> 8,962</li>
-                                            <li class="post-like ms-5"><span>Like:</span> 78K</li>
-                                        </ul>
-                                    </li>
-                                </ul>
-                                <h4 class="title"><a href="blog-details.html">Marcus rashford’s food poverty</a></h4>
-                                <p>Lorem Ipsum is simply dummy textes of them printing and typesetting industry standard</p>
-                            </div>
-                        </div>
-                    </div> -->
-                    <!--== End: News Post Item ==-->
+                    </div>       <!--== End: Testimonial Title ==-->
                 </div>
             </div>
         </div>
->>>>>>> 0ca389c652f3e462c3e31a2668dea60f447e09dc
         <!--== End: News Post Section Wrapper ==-->
 
         <!--== Start: Divider Section Wrapper ==-->
