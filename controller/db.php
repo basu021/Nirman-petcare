f6f4662739e391bf59212766849d796abff2d955
santos021 <santosbiswal543@gmail.com>
2023-02-17 07:28:46 +0530

    ..

--- controller/db.php
+++ controller/db.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "silicon";
+$dbname = "sillicon";
 
 // Create a connection to the MySQL database
 $conn = mysqli_connect($servername, $username, $password, $dbname);
